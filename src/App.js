d16af7c77f1d9e52bc7960720ec15f6c88227fc0
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-10-06 08:12:29 -0500

    Add Ant Design framework to the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Card, DatePicker, Alert } from 'antd';
 
 function App() {
+  const date_test = (date, dateString) => {
+    console.log(date, dateString)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Componentes Ant Design</h2>
+      <label>Ingenieria de Software II</label>
+      <div>
+        <Alert message='Success text' type='success' />
+        <Alert message='Info text' type='info' />
+        <Alert message='Warning text' type='warning' />
+        <Alert message='Error text' type='error' />
+      </div>
+      <label>Seleccione una fecha</label>
+      <DatePicker on onChange={date_test}></DatePicker>
+      <div>
+        <Card
+          size='small'
+          title='Tarjeta de presentacion'
+          extra={<a href="#">Ver mas</a>}
+          style={{ width:300 }}>
+            <p>Juan Felipe Herrera</p>
+            <p>Estudiante Ingenieria de Sistemas</p>
+        </Card>
+      </div>
     </div>
   );
 }
